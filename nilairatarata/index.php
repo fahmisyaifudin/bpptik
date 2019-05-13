1eaa0b973fa04055340fa9076c34deb31ff8700e
fikifh <fikiirmansyah@gmail.com>
2019-05-13 16:59:48 +0700

    Tugas2

---
+++ nilairatarata/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+	<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nama</th>
+      <th scope="col">Nilai</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><input type="text" name="nama[]"></td>
+      <td><input type="number" name="nilai[]"></td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
